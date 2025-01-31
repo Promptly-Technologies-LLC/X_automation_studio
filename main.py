77ef71eea4e827797b779a35cc2c903f99ed7b21
Christopher C. Smith <chriscarrollsmith@gmail.com>
2025-01-30 21:07:49 -0500

    minor

--- main.py
+++ main.py
@@ -209,8 +209,7 @@ async def get_tweet_suggestion(
         }
     )
 
-# To run this app:
-#   uvicorn tweet:app --host 0.0.0.0 --port 5000
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5000)
